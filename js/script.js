c183fe0f04dc7e1b26dc0aa4032e8f5150f1dfeb
Monish Gupta <monish.gupta1@gmail.com>
2017-03-07 18:16:32 +0530

    Revert "Added street view image. Loaded NY Times articles from API"
    This reverts commit 18ea09a285a8534441b6f48dd52e7db04d52de46.

--- js/script.js
+++ js/script.js
@@ -11,45 +11,9 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    /**
-     * load streetview
-     */
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
+    // load streetview
 
-    $greeting.text('So, you want to live at ' + address + '?');
-
-    var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview' + 
-        '?size=600x300&location=' + address;
-    $body.append('<img class="bgimg" src="' + streetViewUrl + '"/>');
-
-    /**
-     * NYTimes data and rendering
-     */
-    var nytUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    nytUrl += '?' + $.param({
-        'api-key': "8ae608650f8b4e508d3abb94cb3ee105",
-        'q': address
-    });
-    $.getJSON(nytUrl, function(data){
-        // todo null checks
-        var articles = data.response.docs;
-        articles.forEach(function(elem, index){
-            var listItem = 
-                `<li class="article">
-                    <a href=${elem.web_url}>
-                        <h1>${elem.headline.main || ''}</h1>
-                    </a>
-                    <p>${elem.lead_paragraph || ''}</p>
-                </li>`;
-            $('#nytimes-articles').append(listItem);
-        });
-    }).done(function(result) {
-        console.log(result);
-    }).fail(function(err) {
-        throw err;
-    });
+    // YOUR CODE GOES HERE!
 
     return false;
 };
